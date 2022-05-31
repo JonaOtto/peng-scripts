c14277578d24a2cc2faccc203522e2342ed9b667
Jonathan Otto <jonathan.otto.37@stud.tu-darmstadt.de>
2022-05-31 13:07:44 +0200

    Updated CVR Run.

--- Analyzer/gprof_analyzer.py
+++ Analyzer/gprof_analyzer.py
@@ -4,23 +4,38 @@ class _FlatProfileEntry:
     """
     Entry for flat profile.
     """
-    def __init__(self):
-        pass
+    def __init__(self, percentage_total, cumulated_secs, self_secs, calls_to_this, self_ms_calls, cumulated_ms_calls, name):
+        self.percentage_total = percentage_total
+        self.cumulated_secs = cumulated_secs
+        self.self_secs = self_secs
+        self.calls_to_this = calls_to_this
+        self.self_ms_calls = self_ms_calls
+        self.cumulated_ms_calls = cumulated_ms_calls
+        self.name = name
 
 
 class _CallGraphNode:
     """
     Node for the call graph.
     """
-    def __init__(self):
-        pass
+    def __init__(self, index, total_time_percentage, self_time, child_time, called, name, parent_index):
+        """
+        Constructor.
+        """
+        self.index = index
+        self.total_time_percentage = total_time_percentage
+        self.self_time = self_time
+        self.child_time = child_time
+        self.called = called
+        self.name = name
+        self.parent_index = parent_index
 
 
 class GProfAnalyzer:
     """
     Analyzer for GProf results.
     """
-    def __init__(self, profile: str):
+    def __init__(self, profile: str, threshold_percentage: float = 5.0):
         """
         Constructor.
         """
@@ -32,11 +47,13 @@ class GProfAnalyzer:
         """
         Reads the profile file into variables.
         """
+        pass
 
     def analyze(self):
         """
         Analyze the results.
         """
+        self.read_file()
 
 
 # temporary for testing
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from Runs.run import App, Resolution, GProfRun, CompilerVectorizationReportRun
 
 if __name__ == '__main__':
-    run = GProfRun(
-        app=App.ISSM_MINIAPP_THERMAL,
-        resolution=Resolution.G16000,
-        # own_build=False,
+    # run = GProfRun(
+    #     app=App.ISSM_MINIAPP_THERMAL,
+    #     resolution=Resolution.G16000,
+    #     # own_build=False,
+    # )
+    # run.do_run()
+    run = CompilerVectorizationReportRun(
+        app=App.ISSM_MINIAPP_STRESSBALANCE,
+        resolution=Resolution.G16000
     )
-    run.do_run()
     print("FINISHED!!!!!")
 
 
