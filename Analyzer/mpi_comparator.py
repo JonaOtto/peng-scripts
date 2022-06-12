73eaaf1cef494a8ae6923c17fa5ae85205a57513
Jonathan Otto <jonathan.otto.37@stud.tu-darmstadt.de>
2022-06-12 13:54:50 +0200

    Updated run.

--- Analyzer/mpi_comparator.py
+++ Analyzer/mpi_comparator.py
@@ -1,18 +1,37 @@
-#import matplotlib as mpl
+# import matplotlib as mpl  # FIXME See below
 
 
 class MPIComparator:
     def __init__(self, analyzer_results):
         self.analyzer_results = analyzer_results
         self.results = {}
+        self.intermediate_res = []
 
     def calc_speedups(self):
-        pass
+        # sort them by cores
+        for key, item in self.analyzer_results.items():
+            self.intermediate_res.append((key, item))
+        self.intermediate_res = sorted(self.intermediate_res, key=lambda x: x[0])
+        # calculate speedups
+        self.results["mpi_compare"] = {}
+        for cores, results in self.intermediate_res:
+            if (cores, results) == self.intermediate_res[0]:
+                # first is baseline. Optimal this should be a result with one core
+                self.results["mpi_compare"][str(cores)] = {"calculation_time": results["calculation_time"]}
+            else:
+                # Speedup = time(1 core)/time(p processors)
+                speedup = results["calculation_time"]/self.intermediate_res[0][1]["calculation_time"]
+                self.results["mpi_compare"][str(cores)] = {"calculation_time": results["calculation_time"], "speedup_to_first": speedup}
+        self.results["mpi_compare"]["average"] = sum([ct[1]["calculation_time"] for ct in self.intermediate_res])/len(self.intermediate_res)
 
     def export_graph(self):
+        # FIXME: The whole programm fails, if runned on an venv. But: We need a venv for
+        # FIXME ... matplotlib! How? Why is this happening? I guess something with subprocesses..
         pass
 
     def analyze(self):
         print("MPI Compare!!")
         print(self.analyzer_results)
+        self.calc_speedups()
+        print(self.results)
         return self.results
--- main.py
+++ main.py
@@ -3,62 +3,78 @@ from Management.experiment import Experiment
 
 if __name__ == '__main__':
 
-    r = Experiment(name="GPROF_BOTH_MINIAPPS_G16000")
-    # gprof miniapps
-    r.add_run(GProfRun(
-        app=App.ISSM_MINIAPP_THERMAL,
-        resolution=Resolution.G16000,
-        cleanup_build=False
-    ))
-    r.add_run(GProfRun(
-        app=App.ISSM_MINIAPP_STRESSBALANCE,
-        resolution=Resolution.G16000,
-        own_build=False
-    ))
-    r.do_run()
+    # r = Experiment(name="GPROF_BOTH_MINIAPPS_G16000")
+    # # gprof miniapps
+    # r.add_run(GProfRun(
+    #     app=App.ISSM_MINIAPP_THERMAL,
+    #     resolution=Resolution.G16000,
+    #     cleanup_build=False
+    # ))
+    # r.add_run(GProfRun(
+    #     app=App.ISSM_MINIAPP_STRESSBALANCE,
+    #     resolution=Resolution.G16000,
+    #     own_build=False
+    # ))
+    # r.do_run()
+    #
+    # # ALL Apps gprof:
+    #
+    # r = Experiment(name="GPROF_ALL_APPS_G16000")
+    # # make vanilla runs to compare
+    # r.add_run(BaseRun(
+    #     app=App.ISSM_MINIAPP_STRESSBALANCE,
+    #     resolution=Resolution.G16000,
+    #     cleanup_build=False
+    # ))
+    # r.add_run(BaseRun(
+    #     app=App.ISSM_MINIAPP_THERMAL,
+    #     resolution=Resolution.G16000,
+    #     own_build=False,
+    #     cleanup_build=False
+    # ))
+    # # gprof miniapps
+    # r.add_run(GProfRun(
+    #     app=App.ISSM_MINIAPP_THERMAL,
+    #     resolution=Resolution.G16000,
+    #     own_build=False,
+    #     cleanup_build=False
+    # ))
+    # r.add_run(GProfRun(
+    #     app=App.ISSM_MINIAPP_STRESSBALANCE,
+    #     resolution=Resolution.G16000,
+    #     own_build=False,
+    #     cleanup_build=False
+    # ))
+    # # run the plain app to compare
+    # r.add_run(GProfRun(
+    #     app=App.ISSM_4_18,
+    #     resolution=Resolution.G16000,
+    #     cleanup_build=False
+    # ))
+    # r.do_run()
+    #
+    # r = Experiment(name="Callgrind-Test")
+    # r.add_run(CallgrindRun(
+    #     app=App.ISSM_MINIAPP_THERMAL,
+    #     resolution=Resolution.G64000,
+    #     own_build=False,
+    # ))
+    # r.do_run()
 
-    # ALL Apps gprof:
-
-    r = Experiment(name="GPROF_ALL_APPS_G16000")
-    # make vanilla runs to compare
-    r.add_run(BaseRun(
-        app=App.ISSM_MINIAPP_STRESSBALANCE,
-        resolution=Resolution.G16000,
-        cleanup_build=False
-    ))
-    r.add_run(BaseRun(
-        app=App.ISSM_MINIAPP_THERMAL,
-        resolution=Resolution.G16000,
-        own_build=False,
-        cleanup_build=False
-    ))
-    # gprof miniapps
-    r.add_run(GProfRun(
-        app=App.ISSM_MINIAPP_THERMAL,
-        resolution=Resolution.G16000,
-        own_build=False,
-        cleanup_build=False
-    ))
-    r.add_run(GProfRun(
-        app=App.ISSM_MINIAPP_STRESSBALANCE,
-        resolution=Resolution.G16000,
-        own_build=False,
-        cleanup_build=False
-    ))
-    # run the plain app to compare
-    r.add_run(GProfRun(
-        app=App.ISSM_4_18,
-        resolution=Resolution.G16000,
-        cleanup_build=False
-    ))
-    r.do_run()
-
-    r = Experiment(name="Callgrind-Test")
-    r.add_run(CallgrindRun(
-        app=App.ISSM_MINIAPP_THERMAL,
-        resolution=Resolution.G64000,
-        own_build=False,
-    ))
+    r = Experiment(name="MPI-SCALE-TEST")
+    i = 3
+    r.add_run(
+        MPIRun(app=App.ISSM_MINIAPP_THERMAL,
+               resolution=Resolution.G16000,
+               num_mpi_ranks=1)
+    )
+    while i <= 96:
+        r.add_run(
+            MPIRun(app=App.ISSM_MINIAPP_THERMAL,
+                   resolution=Resolution.G16000,
+                   num_mpi_ranks=i)
+        )
+        i = i * 2
     r.do_run()
 
     print("FINISHED!!!!!")
