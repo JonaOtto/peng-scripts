e469ce03a9e0bf3e216748d8a87b9bd73fabfff4
Jonathan Otto <jonathan.otto.37@stud.tu-darmstadt.de>
2022-06-13 10:57:44 +0200

    Updated run.

--- main.py
+++ main.py
@@ -53,29 +53,44 @@ if __name__ == '__main__':
     # ))
     # r.do_run()
     #
-    # r = Experiment(name="Callgrind-Test")
+
+    e = Experiment(name="MINIAPPS-PLAIN-G16000")
+    e.add_run(BaseRun(app=App.ISSM_MINIAPP_THERMAL, resolution=Resolution.G16000, cleanup_build=False))
+    e.do_run()
+
+    # r = Experiment(name="CALLGRIND-G16000")
     # r.add_run(CallgrindRun(
     #     app=App.ISSM_MINIAPP_THERMAL,
-    #     resolution=Resolution.G64000,
-    #     own_build=False,
+    #     resolution=Resolution.G16000,
+    # ))
+    # r.add_run(CallgrindRun(
+    #     app=App.ISSM_MINIAPP_THERMAL,
+    #     resolution=Resolution.G16000,
+    #     cache_sim=True,
+    # ))
+    # r.add_run(CallgrindRun(
+    #     app=App.ISSM_MINIAPP_THERMAL,
+    #     resolution=Resolution.G16000,
+    #     branch_sim=True,
     # ))
     # r.do_run()
 
-    r = Experiment(name="MPI-SCALE-TEST")
-    i = 3
-    r.add_run(
-        MPIRun(app=App.ISSM_MINIAPP_THERMAL,
-               resolution=Resolution.G16000,
-               num_mpi_ranks=1)
-    )
-    while i <= 96:
-        r.add_run(
-            MPIRun(app=App.ISSM_MINIAPP_THERMAL,
-                   resolution=Resolution.G16000,
-                   num_mpi_ranks=i)
-        )
-        i = i * 2
-    r.do_run()
+
+    # r = Experiment(name="MPI-SCALE-TEST")
+    # i = 3
+    # r.add_run(
+    #     MPIRun(app=App.ISSM_MINIAPP_THERMAL,
+    #            resolution=Resolution.G16000,
+    #            num_mpi_ranks=1)
+    # )
+    # while i <= 96:
+    #     r.add_run(
+    #         MPIRun(app=App.ISSM_MINIAPP_THERMAL,
+    #                resolution=Resolution.G16000,
+    #                num_mpi_ranks=i)
+    #     )
+    #     i = i * 2
+    # r.do_run()
 
     print("FINISHED!!!!!")
 
