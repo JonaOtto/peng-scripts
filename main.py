bcc67187ae119a834c58138436f6f1cbc36c1d12
Jonathan Otto <jonathan.otto.37@stud.tu-darmstadt.de>
2022-06-13 12:21:28 +0200

    Updated run.

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ if __name__ == '__main__':
     # r.add_run(GProfRun(
     #     app=App.ISSM_MINIAPP_THERMAL,
     #     resolution=Resolution.G16000,
-    #     cleanup_build=False
+    #     cleanup_build=False,
+    #
     # ))
     # r.add_run(GProfRun(
     #     app=App.ISSM_MINIAPP_STRESSBALANCE,
@@ -17,80 +18,80 @@ if __name__ == '__main__':
     # ))
     # r.do_run()
     #
-    # # ALL Apps gprof:
-    #
-    # r = Experiment(name="GPROF_ALL_APPS_G16000")
-    # # make vanilla runs to compare
-    # r.add_run(BaseRun(
-    #     app=App.ISSM_MINIAPP_STRESSBALANCE,
-    #     resolution=Resolution.G16000,
-    #     cleanup_build=False
-    # ))
-    # r.add_run(BaseRun(
-    #     app=App.ISSM_MINIAPP_THERMAL,
-    #     resolution=Resolution.G16000,
-    #     own_build=False,
-    #     cleanup_build=False
-    # ))
-    # # gprof miniapps
-    # r.add_run(GProfRun(
-    #     app=App.ISSM_MINIAPP_THERMAL,
-    #     resolution=Resolution.G16000,
-    #     own_build=False,
-    #     cleanup_build=False
-    # ))
-    # r.add_run(GProfRun(
-    #     app=App.ISSM_MINIAPP_STRESSBALANCE,
-    #     resolution=Resolution.G16000,
-    #     own_build=False,
-    #     cleanup_build=False
-    # ))
-    # # run the plain app to compare
-    # r.add_run(GProfRun(
-    #     app=App.ISSM_4_18,
-    #     resolution=Resolution.G16000,
-    #     cleanup_build=False
-    # ))
-    # r.do_run()
-    #
+    # ALL Apps gprof:
+
+    r = Experiment(name="GPROF_ALL_APPS_G16000")
+    # make vanilla runs to compare
+    r.add_run(BaseRun(
+        app=App.ISSM_MINIAPP_STRESSBALANCE,
+        resolution=Resolution.G16000,
+        cleanup_build=False
+    ))
+    r.add_run(BaseRun(
+        app=App.ISSM_MINIAPP_THERMAL,
+        resolution=Resolution.G16000,
+        own_build=False,
+        cleanup_build=False
+    ))
+    # gprof miniapps
+    r.add_run(GProfRun(
+        app=App.ISSM_MINIAPP_THERMAL,
+        resolution=Resolution.G16000,
+        own_build=False,
+        cleanup_build=False
+    ))
+    r.add_run(GProfRun(
+        app=App.ISSM_MINIAPP_STRESSBALANCE,
+        resolution=Resolution.G16000,
+        own_build=False,
+        cleanup_build=False
+    ))
+    # run the plain app to compare
+    r.add_run(GProfRun(
+        app=App.ISSM_4_18,
+        resolution=Resolution.G16000,
+        cleanup_build=False
+    ))
+    r.do_run()
+
 
     # e = Experiment(name="MINIAPPS-PLAIN-G16000")
     # e.add_run(BaseRun(app=App.ISSM_MINIAPP_THERMAL, resolution=Resolution.G16000, cleanup_build=False))
     # e.do_run()
 
-    r = Experiment(name="CALLGRIND-G16000")
-    r.add_run(CallgrindRun(
-        app=App.ISSM_MINIAPP_THERMAL,
-        resolution=Resolution.G16000,
-    ))
-    r.add_run(CallgrindRun(
-        app=App.ISSM_MINIAPP_THERMAL,
-        resolution=Resolution.G16000,
-        cache_sim=True,
-    ))
-    r.add_run(CallgrindRun(
-        app=App.ISSM_MINIAPP_THERMAL,
-        resolution=Resolution.G16000,
-        branch_sim=True,
-    ))
-    r.do_run()
-
-    r = Experiment(name="CALLGRIND-G16000")
-    r.add_run(CallgrindRun(
-        app=App.ISSM_MINIAPP_STRESSBALANCE,
-        resolution=Resolution.G16000,
-    ))
-    r.add_run(CallgrindRun(
-        app=App.ISSM_MINIAPP_STRESSBALANCE,
-        resolution=Resolution.G16000,
-        cache_sim=True,
-    ))
-    r.add_run(CallgrindRun(
-        app=App.ISSM_MINIAPP_STRESSBALANCE,
-        resolution=Resolution.G16000,
-        branch_sim=True,
-    ))
-    r.do_run()
+    # r = Experiment(name="CALLGRIND-G16000")
+    # r.add_run(CallgrindRun(
+    #     app=App.ISSM_MINIAPP_THERMAL,
+    #     resolution=Resolution.G16000,
+    # ))
+    # r.add_run(CallgrindRun(
+    #     app=App.ISSM_MINIAPP_THERMAL,
+    #     resolution=Resolution.G16000,
+    #     cache_sim=True,
+    # ))
+    # r.add_run(CallgrindRun(
+    #     app=App.ISSM_MINIAPP_THERMAL,
+    #     resolution=Resolution.G16000,
+    #     branch_sim=True,
+    # ))
+    # r.do_run()
+    #
+    # r = Experiment(name="CALLGRIND-G16000")
+    # r.add_run(CallgrindRun(
+    #     app=App.ISSM_MINIAPP_STRESSBALANCE,
+    #     resolution=Resolution.G16000,
+    # ))
+    # r.add_run(CallgrindRun(
+    #     app=App.ISSM_MINIAPP_STRESSBALANCE,
+    #     resolution=Resolution.G16000,
+    #     cache_sim=True,
+    # ))
+    # r.add_run(CallgrindRun(
+    #     app=App.ISSM_MINIAPP_STRESSBALANCE,
+    #     resolution=Resolution.G16000,
+    #     branch_sim=True,
+    # ))
+    # r.do_run()
 
 
     # r = Experiment(name="MPI-SCALE-TEST")
