8805d6bc19af0ed893676b64fb9c8b640367cb20
Jonathan Otto <jonathan.otto.37@stud.tu-darmstadt.de>
2022-06-13 15:18:06 +0200

    Updated main.

--- main.py
+++ main.py
@@ -20,39 +20,39 @@ if __name__ == '__main__':
     #
     # ALL Apps gprof:
 
-    r = Experiment(name="GPROF_ALL_APPS_G16000")
-    # make vanilla runs to compare
-    r.add_run(BaseRun(
-        app=App.ISSM_MINIAPP_STRESSBALANCE,
-        resolution=Resolution.G16000,
-        cleanup_build=False
-    ))
-    r.add_run(BaseRun(
-        app=App.ISSM_MINIAPP_THERMAL,
-        resolution=Resolution.G16000,
-        own_build=False,
-        cleanup_build=False
-    ))
-    # gprof miniapps
-    r.add_run(GProfRun(
-        app=App.ISSM_MINIAPP_THERMAL,
-        resolution=Resolution.G16000,
-        own_build=False,
-        cleanup_build=False
-    ))
-    r.add_run(GProfRun(
-        app=App.ISSM_MINIAPP_STRESSBALANCE,
-        resolution=Resolution.G16000,
-        own_build=False,
-        cleanup_build=False
-    ))
-    # run the plain app to compare
-    r.add_run(GProfRun(
-        app=App.ISSM_4_18,
-        resolution=Resolution.G16000,
-        cleanup_build=False
-    ))
-    r.do_run()
+    # r = Experiment(name="GPROF_ALL_APPS_G16000")
+    # # make vanilla runs to compare
+    # r.add_run(BaseRun(
+    #     app=App.ISSM_MINIAPP_STRESSBALANCE,
+    #     resolution=Resolution.G16000,
+    #     cleanup_build=False
+    # ))
+    # r.add_run(BaseRun(
+    #     app=App.ISSM_MINIAPP_THERMAL,
+    #     resolution=Resolution.G16000,
+    #     own_build=False,
+    #     cleanup_build=False
+    # ))
+    # # gprof miniapps
+    # r.add_run(GProfRun(
+    #     app=App.ISSM_MINIAPP_THERMAL,
+    #     resolution=Resolution.G16000,
+    #     own_build=False,
+    #     cleanup_build=False
+    # ))
+    # r.add_run(GProfRun(
+    #     app=App.ISSM_MINIAPP_STRESSBALANCE,
+    #     resolution=Resolution.G16000,
+    #     own_build=False,
+    #     cleanup_build=False
+    # ))
+    # # run the plain app to compare
+    # r.add_run(GProfRun(
+    #     app=App.ISSM_4_18,
+    #     resolution=Resolution.G16000,
+    #     cleanup_build=False
+    # ))
+    # r.do_run()
 
     e = Experiment(name="VEC_REPORT_MINIAPPS")
     e.add_run(CompilerVectorizationReportRun(
