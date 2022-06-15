c2f0a6daa41efb26e11672a2918754c1ff456c03
Jonathan Otto <jonathan.otto.37@stud.tu-darmstadt.de>
2022-06-15 09:01:44 +0200

    Updated main.

--- main.py
+++ main.py
@@ -2,19 +2,24 @@ from Runs.run import *
 from Management.experiment import Experiment
 
 if __name__ == '__main__':
+    e = Experiment(name="MINIAPPS-PLAIN-G16000")
+    e.add_run(BaseRun(app=App.ISSM_MINIAPP_THERMAL, resolution=Resolution.G16000, own_build=True, cleanup_build=True))
+    e.add_run(BaseRun(app=App.ISSM_MINIAPP_STRESSBALANCE, resolution=Resolution.G16000, own_build=True, cleanup_build=True))
+    e.do_run()
 
     r = Experiment(name="GPROF_BOTH_MINIAPPS_G16000")
     # gprof miniapps
     r.add_run(GProfRun(
         app=App.ISSM_MINIAPP_THERMAL,
         resolution=Resolution.G16000,
+        own_build=True,
         cleanup_build=False,
-
     ))
     r.add_run(GProfRun(
         app=App.ISSM_MINIAPP_STRESSBALANCE,
         resolution=Resolution.G16000,
-        own_build=False
+        own_build=False,
+        cleanup_build=True
     ))
     r.do_run()
     #
@@ -54,30 +59,27 @@ if __name__ == '__main__':
     # ))
     # r.do_run()
 
-    # e = Experiment(name="VEC_REPORT_MINIAPPS")
-    # e.add_run(CompilerVectorizationReportRun(
-    #     app=App.ISSM_MINIAPP_THERMAL,
-    #     resolution=Resolution.G16000,
-    #     own_build=True,
-    #     cleanup_build=True,
-    # ))
-    # e.add_run(CompilerVectorizationReportRun(
-    #     app=App.ISSM_MINIAPP_STRESSBALANCE,
-    #     resolution=Resolution.G16000,
-    #     own_build=True,
-    #     cleanup_build=True,
-    # ))
-    # e.do_run()
+    e = Experiment(name="VEC_REPORT_MINIAPPS")
+    e.add_run(CompilerVectorizationReportRun(
+        app=App.ISSM_MINIAPP_THERMAL,
+        resolution=Resolution.G16000,
+        own_build=True,
+        cleanup_build=True,
+    ))
+    e.add_run(CompilerVectorizationReportRun(
+        app=App.ISSM_MINIAPP_STRESSBALANCE,
+        resolution=Resolution.G16000,
+        own_build=True,
+        cleanup_build=True,
+    ))
+    e.do_run()
 
     # e = Experiment(name="SCORE-P-RUNS")
     # e.add_run(ScorePRun(app=App.ISSM_MINIAPP_THERMAL, resolution=Resolution.G16000))
     # e.add_run(ScorePRun(app=App.ISSM_MINIAPP_STRESSBALANCE, resolution=Resolution.G16000))
     # e.do_run()
 
-    # e = Experiment(name="MINIAPPS-PLAIN-G16000")
-    # e.add_run(BaseRun(app=App.ISSM_MINIAPP_THERMAL, resolution=Resolution.G16000, cleanup_build=False))
-    # e.add_run(BaseRun(app=App.ISSM_MINIAPP_STRESSBALANCE, resolution=Resolution.G16000, cleanup_build=False))
-    # e.do_run()
+
 
     # r = Experiment(name="CALLGRIND-G16000")
     # r.add_run(CallgrindRun(
